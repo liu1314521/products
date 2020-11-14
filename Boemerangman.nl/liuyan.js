52b90968d28cad288293ac11505812381e2c91ff
liu1314521 <40618013+liu1314521@users.noreply.github.com>
2020-11-14 16:39:52 +0800

    Create liuyan.js

---
+++ Boemerangman.nl/liuyan.js
@@ -0,0 +1,160 @@
+document.write(" <ul class=\"Fum198_RelPro\">");
+document.write("                <li>");
+document.write("                    <h3><a rel=\"nofollow\" href=\"\/solution\/stone-crushing-production-line.html\">Stone Crushing Production Line<\/a><\/h3>");
+document.write("                    <span class=\"bubble-line\"><\/span>");
+document.write("                    <a rel=\"nofollow\" href=\"\/solution\/stone-crushing-production-line.html\" class=\"Fum198_RelProPic\">");
+document.write("                        <img src=\"\/images\/cx_small_20180512140951.jpg\" alt=\"Stone Crushing Production Line\" \/><\/a>");
+document.write("                    <p>Stone crushing plant mainly consists of vibrating feeder, jaw Crusher, impact crusher, vibrating screen, belt conveyor, centralized electronic control components, and the designed production capacity is 50-800t\/h.<\/p>");
+document.write("                <\/li>");
+document.write("                <li>");
+document.write("                    <h3><a rel=\"nofollow\" href=\"\/solution\/powder-grinding-production-line.html\">Powder Grinding Production Line<\/a><\/h3>");
+document.write("                    <span class=\"bubble-line\"><\/span>");
+document.write("                    <a rel=\"nofollow\" href=\"\/solution\/powder-grinding-production-line.html\" class=\"Fum198_RelProPic\">");
+document.write("                        <img src=\"\/images\/cx_small_20180512141042.jpg\" alt=\"Powder Grinding Production Line\" \/><\/a>");
+document.write("                    <p>Sand making plant is a kind of special equipment for producing building sand and stone.<\/p>");
+document.write("                <\/li>");
+document.write("                <li>");
+document.write("                    <h3><a rel=\"nofollow\" href=\"\/solution\/sand-making-production-line.html\">Sand Making Production Line<\/a><\/h3>");
+document.write("                    <span class=\"bubble-line\"><\/span>");
+document.write("                    <a rel=\"nofollow\" href=\"\/solution\/sand-making-production-line.html\" class=\"Fum198_RelProPic\">");
+document.write("                        <img src=\"\/images\/cx_small_20180512141004.jpg\" \/><\/a>");
+document.write("                    <p>fote new magnetic separator plant has advantages of high efficiency, low energy, high capacity and so on.<\/p>");
+document.write("                <\/li>");
+document.write("");
+document.write("                <li>");
+document.write("                    <h3><a rel=\"nofollow\" href=\"\/solution\/active-lime-production-line.html\">Active Lime Production Line<\/a><\/h3>");
+document.write("                    <span class=\"bubble-line\"><\/span>");
+document.write("                    <a rel=\"nofollow\" href=\"\/solution\/active-lime-production-line.html\" class=\"Fum198_RelProPic\">");
+document.write("                        <img src=\"\/images\/cx_small_20170726181057.jpg\" alt=\"Active Lime Production Line\" \/><\/a>");
+document.write("                    <p>Grinding plant is mainly used in powder making from non metallic materials or other medium-hard materials.<\/p>");
+document.write("                <\/li>");
+document.write("");
+document.write("                <li>");
+document.write("                    <h3><a rel=\"nofollow\" href=\"\/solution\/cement-plant-production-line.html\">Cement Plant Production Line<\/a><\/h3>");
+document.write("                    <span class=\"bubble-line\"><\/span>");
+document.write("                    <a rel=\"nofollow\" href=\"\/solution\/cement-plant-production-line.html\" class=\"Fum198_RelProPic\">");
+document.write("                        <img src=\"\/images\/cx_small_20180512141107.jpg\" alt=\"Cement Plant Production Line\" \/><\/a>");
+document.write("                    <p>Fote active lime production line has reached full automatic production from auto feeding raw materials to producing finished active lime.<\/p>");
+document.write("                <\/li>");
+document.write("");
+document.write("                <li>");
+document.write("                    <h3><a rel=\"nofollow\" href=\"\/solution\/ore-beneficiation-plant.html\">Ore Beneficiation Plant<\/a><\/h3>");
+document.write("                    <span class=\"bubble-line\"><\/span>");
+document.write("                    <a rel=\"nofollow\" href=\"\/solution\/ore-beneficiation-plant.html\" class=\"Fum198_RelProPic\">");
+document.write("                        <img src=\"\/images\/cx_small_20180629113818.jpg\" alt=\"Ore Beneficiation Plant\" \/><\/a>");
+document.write("                    <p>New dry-process cement plant is composed of raw materials crushing equipment, grinding equipment, cement grinding equipment, cement rotary kiln and other equipment.<\/p>");
+document.write("                <\/li>");
+document.write("            <\/ul>");
+document.write("  <div class=\"lyb-msg m-top p-padding\" id=\"liuyanban\">");
+document.write("                <div class=\"msgfl\">");
+document.write("                    <div class=\"mag-top\">");
+document.write("                        <p class=\"p1\">");
+document.write("                            Leave Us Message");
+document.write("                        <\/p>");
+document.write("                        <p class=\"p2\">30s quick response service <span><i><\/i><\/span><\/p>");
+document.write("                    <\/div>");
+document.write("                    <p class=\"p3\">We not only provide a good product, but also provide high quality service. If you are interested in our products,you can contact us in the following ways.<\/p>");
+document.write("<form id=\"Inquiry\" name=\"Inquiry\" class=\"send\" method=\"post\" action=\"https:\/\/q.kssbchina.com\/information\/\" onsubmit=\"return doSubmits()\">");
+document.write("    <div class=\"input-panel\">");
+document.write("        <div class=\"me one\">");
+document.write("            <strong>Product Name: <\/strong>");
+document.write("            <input id=\"productname\" name=\"productname\" class=\"txt icon-text-pro\" placeholder=\"Product Name\" type=\"text\">");
+document.write("        <\/div>");
+document.write("        <div class=\"me two\">");
+document.write("            <strong>Your Name:<\/strong>");
+document.write("            <input id=\"name\" name=\"name\" class=\"txt icon-text-user\" placeholder=\"Your Name\" type=\"text\">");
+document.write("        <\/div>");
+document.write("        <div class=\"me three\">");
+document.write("            <strong>Your Email(<span style=\"margin: 0px; color: red\">*<\/span>)<\/strong>");
+document.write("            <input id=\"email\" name=\"email\" class=\"txt icon-text-email\" placeholder=\"Your Email\" type=\"text\">");
+document.write("        <\/div>");
+document.write("        <div class=\"me four\">");
+document.write("            <strong>Your Phone or Whatsapp:<\/strong>");
+document.write("            <input id=\"telephone\" name=\"telephone\" class=\"txt icon-text-tel\" placeholder=\"Your Phone or Whatsapp\" type=\"text\">");
+document.write("        <\/div>");
+document.write("    <\/div>");
+document.write("    <div class=\"me five\">");
+document.write("        <strong>Message(<span style=\"margin: 0px; color: red\"> as specific as possible*<\/span>)<\/strong>");
+document.write("        <textarea id=\"message\" class=\"textarea icon-text-message\" name=\"message\" placeholder=\"Equipment name, model, related issues\"><\/textarea>");
+document.write("    <\/div>");
+document.write("    <input id=\"site_feedback\" type=\"hidden\" name=\"site_feedback\">");
+document.write("    <input id=\"from_way\" type=\"hidden\" name=\"from_way\">");
+document.write("    <input id=\"refer\" type=\"hidden\" name=\"refer\">");
+document.write("    <div class=\"PorConSixXX\">");
+document.write("        <p>");
+document.write("            <input type=\"checkbox\" checked=\"checked\" class=\"choose_input\"><\/p>");
+document.write("        I accept the Data Protection Declaration");
+document.write("    <\/div>");
+document.write("    <input id=\"button_sub\" name=\"button_sub\" class=\"btn-primary\" type=\"submit\" value=\"Send Now!\">");
+document.write("<\/form>");
+document.write("                <\/div>");
+document.write("            <\/div>");
+
+function doSubmits() {
+    var stremail = document.getElementById("email");
+    if (stremail.value == "") {
+        alert('Please write down your correct Email');
+        stremail.focus();
+        return false;
+    } else {
+        var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+        isok = reg.test(stremail.value);
+        if (!isok) {
+            alert("The format of the e-mail is incorrect, please re-enter the e-mail！");
+            stremail.focus();
+            return false;
+        }
+    }
+
+    var smessage = document.getElementById("message");
+    if (smessage.value == "") {
+        smessage.style.backgroundColor = "#bbc0c6";
+        alert('Please fill in the message');
+        smessage.focus();
+        return false;
+    }
+    var phone = document.getElementById("telephone");
+    if (phone.value !== "") {
+        var regs = /^[0-9]*$/;
+        isok = regs.test(phone.value);
+        leng = phone.value.length;
+        if (!isok || leng>20) {
+            alert("Please fill in the correct mobile number or Whatsapp;");
+            phone.focus();
+            return false;
+        }
+    }
+    // 1.客户端浏览器时间
+    var myDate = new Date();
+    var datenow = myDate.toLocaleDateString() + "-" + myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+    //2. 随即字符串
+    var str = '';
+    var length = 10;
+    for (; str.length < length; str += Math.random().toString(36).substr(2));
+    str = str.substr(0, length);
+
+    //3.移动版
+    var mobileAgent = new Array("iphone", "ipod", "ipad", "android", "mobile", "blackberry", "webos", "incognito",
+        "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
+    var browser = navigator.userAgent.toLowerCase();
+    var isMobile = "pc";
+    for (var i = 0; i < mobileAgent.length; i++) {
+        if (browser.indexOf(mobileAgent[i]) != -1) {
+            isMobile = "m";
+            break;
+        }
+    }
+
+    window.document.Inquiry.site_feedback.value = str;
+    window.document.Inquiry.from_way.value = "|" + isMobile + "|";
+    window.document.Inquiry.refer.value = window.location.href + "?data=" + datenow + "&rnd=" + str + "&sys=" +
+        isMobile;
+    window.document.Inquiry.action = window.document.Inquiry.action + "?data=" + datenow + "&rnd=" + str + "&sys=" +
+        isMobile;
+    document.getElementById("button_sub").disabled = true;
+    window.document.Inquiry.submit();
+
+}
+
+
+
